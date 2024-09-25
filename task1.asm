b4186be477c976bef0182929d8be3b45b959a7e7
HenIsTheMan <2169test2169@gmail.com>
2024-09-25 15:15:37 +0800

    Task 1 done (just need task1.asm)
    I see this is what they meant

--- task1.asm
+++ task1.asm
@@ -1,10 +1,29 @@
-# task1.asm
-
 .data
+
+str:
+	.asciiz "the answer = "
 
 .text
 
-# Callee
+main:
+	jal my_func
+	addi $t0, $v0, 0 # Store return val in $t0
+
+	# Print str to console
+	li $v0, 4
+	la $a0, str
+	syscall
+
+	# Print $t0 to console
+	li $v0, 1
+	addi $a0, $t0, 0
+	syscall
+	
+	# Terminate prog
+	li $v0, 10
+	syscall
+
+# Must be below main
 my_func:
     # Read user input
     li $v0, 5   # 5 is system call code for read_int
