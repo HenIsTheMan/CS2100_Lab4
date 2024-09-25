4587401761023bb73cace18de86ee0a8487f16b5
HenIsTheMan <2169test2169@gmail.com>
2024-09-25 15:12:07 +0800

    Revert "[For Ref] not what I wanted"
    This reverts commit 8f2ea7d36db8c41e11de6b893461407ea6bfcc31.

--- messages.asm
+++ messages.asm
@@ -5,26 +5,11 @@
 str:
 	.asciiz "the answer = "
 
-file_name:
-	.asciiz "task1.asm"
-
 .text
 
 main:
-	# Open file
-    li $v0, 13
-    la $a0, file_name
-    li $a1, 1
-    li $a2, 0
-    syscall
-
-	# Store file descriptor in $t1
-	addi $t1, $a0, 0
-
 	jal my_func
-
-	# Store return val in $t0
-	addi $t0, $v0, 0
+	addi $t0, $v0, 0 # Store return val in $t0
 
 	# Print str to console
 	li $v0, 4
@@ -34,11 +19,6 @@ main:
 	# Print $t0 to console
 	li $v0, 1
 	addi $a0, $t0, 0
-	syscall
-
-	# Close file
-	li $v0, 16
-	addi $a0, $t1, 0
 	syscall
 	
 	# Terminate prog
