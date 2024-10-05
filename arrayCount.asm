0310851e4fedbf62ad0e7340c521affc6a1028bb
HenIsTheMan <2169test2169@gmail.com>
2024-10-05 16:20:21 +0800

    Almost done

--- arrayCount.asm
+++ arrayCount.asm
@@ -1,21 +1,29 @@
-# arrayCount.asm
-  .data 
-arrayA: .word 1, 0, 2, 0, 3   # arrayA has 5 values
-count:  .word 999             # dummy value
+.data 
 
-  .text
+arrayA:
+	.word 1, 0, 2, 0, 3, 5, 7, 9
+
+count:
+	.word 999 # 999 is a dummy val
+
+.text
+
 main:
-    # code to setup the variable mappings
-    add $zero, $zero, $zero  #dummy instructions, can be removed
-    add $zero, $zero, $zero  #dummy instructions, can be removed
-    add $zero, $zero, $zero  #dummy instructions, can be removed
+	# code to setup the variable mappings
+	la $t0, arrayA
+	la $t8, count
 
-    # code for reading in the user value X
 
-    # code for counting multiples of X in arrayA
+	add $zero, $zero, $zero  #dummy instructions, can be removed
+	add $zero, $zero, $zero  #dummy instructions, can be removed
+	add $zero, $zero, $zero  #dummy instructions, can be removed
 
-    # code for printing result
+	# code for reading in the user value X
 
-    # code for terminating program
-    li  $v0, 10
-    syscall
+	# code for counting multiples of X in arrayA
+
+	# code for printing result
+
+	# code for terminating program
+	li  $v0, 10
+	syscall
